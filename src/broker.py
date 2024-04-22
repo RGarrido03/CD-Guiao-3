e3260d37edd49e27ca75045194e4c6ac29bafbf6
Rúben Garrido <57329376+RGarrido03@users.noreply.github.com>
2024-04-22 15:56:43 +0100

    Broker initialization
    Co-authored-by: Diogo Falcão <109617044+falcaodiogo@users.noreply.github.com>

--- src/broker.py
+++ src/broker.py
@@ -1,7 +1,7 @@
 """Message Broker"""
 import enum
 import socket
-from typing import Dict, List, Any, Tuple
+from typing import Dict, List, Any, Tuple, Literal, Union
 
 
 class Serializer(enum.Enum):
@@ -10,6 +10,10 @@ class Serializer(enum.Enum):
     JSON = 0
     XML = 1
     PICKLE = 2
+
+
+subscriber_type = tuple[socket.socket, Serializer]
+topic_type = tuple[list[subscriber_type], str]
 
 
 class Broker:
@@ -21,23 +25,61 @@ class Broker:
         self._host = "localhost"
         self._port = 5000
 
+        """
+        {
+          "hello": (
+            [
+              (socket1, Serializer.JSON),
+              (socket2, Serializer.XML),
+              ...
+            ],
+            "abcd"
+          )
+        }
+        
+        JSON equivalent:
+        {
+          "hello": {
+            "subscribers": [
+              {
+                "address": socket1,
+                "serializer": Serializer.JSON
+              },
+              {
+                "address": socket2,
+                "serializer": Serializer.XML
+              },
+              ...
+            ],
+            "last_value": "abcd"
+          }
+        }
+        """
+        self.topics: dict[str, topic_type] = {}
+
     def list_topics(self) -> List[str]:
         """Returns a list of strings containing all topics containing values."""
+        return list(self.topics.keys())
 
-    def get_topic(self, topic):
+    def get_topic(self, topic) -> str:
         """Returns the currently stored value in topic."""
+        return self.topics[topic][1]
 
-    def put_topic(self, topic, value):
+    def put_topic(self, topic: str, value: str):
         """Store in topic the value."""
+        self.topics[topic] = self.topics[topic][0], value
 
     def list_subscriptions(self, topic: str) -> List[Tuple[socket.socket, Serializer]]:
         """Provide list of subscribers to a given topic."""
+        return self.topics[topic][0]
 
     def subscribe(self, topic: str, address: socket.socket, _format: Serializer = None):
         """Subscribe to topic by client in address."""
+        self.topics[topic][0] += [(address, _format)]
 
-    def unsubscribe(self, topic, address):
+    def unsubscribe(self, topic: str, address: socket.socket):
         """Unsubscribe to topic by client in address."""
+        self.topics[topic] = ([client for client in self.topics[topic][0] if client[0] != address], self.topics[topic][1])
 
     def run(self):
         """Run until canceled."""
