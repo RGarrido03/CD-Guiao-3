6bbdf687d7af76f0c018019d44f9c63f940e32ce
Rúben Garrido <57329376+RGarrido03@users.noreply.github.com>
2024-04-27 15:10:27 +0100

    Update CDProtoBadFormat to accept str instead of bytes

--- src/utils.py
+++ src/utils.py
@@ -264,13 +264,13 @@ class CDProto:
 
 
 class CDProtoBadFormat(Exception):
-    """Exception when source message is not CDProto."""
+    """Exception when the source message is not CDProto."""
 
-    def __init__(self, original_msg: bytes = None):
-        """Store original message that triggered exception."""
+    def __init__(self, original_msg: str = None):
+        """Store the original message that triggered exception."""
         self._original = original_msg
 
     @property
     def original_msg(self) -> str:
-        """Retrieve original message as a string."""
-        return self._original.decode("utf-8")
+        """Retrieve the original message as a string."""
+        return self._original
