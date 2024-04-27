cd121cf1906f4157083a0ef2f7cb9a84af318631
Rúben Garrido <57329376+RGarrido03@users.noreply.github.com>
2024-04-27 18:19:20 +0100

    Refactor Queue subclasses' code to make it serializer-abstract

--- src/middleware.py
+++ src/middleware.py
@@ -11,10 +11,12 @@ from src.protocol import CDProto
 class Queue:
     """Representation of Queue interface for both Consumers and Producers."""
 
-    def __init__(self, topic, _type=MiddlewareType.CONSUMER):
+    def __init__(self, topic, serializer: Serializer, _type=MiddlewareType.CONSUMER):
         """Create Queue."""
         self.topic = topic
         self.type = _type
+        self.serializer = serializer
+
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.sock.connect(("localhost", 5000))
 
@@ -24,123 +26,43 @@ class Queue:
 
     def push(self, value):
         """Sends data to broker."""
+        CDProto.send_msg(self.sock, Command.PUBLISH, self.serializer, self.topic, value)
 
     def pull(self) -> Tuple[str, Any]:
         """Receives (topic, data) from broker. Should BLOCK the consumer!"""
         msg = CDProto.recv_msg(self.sock)
-        if msg:  # pub
+        if msg.command == "publish":
             return msg
-        # elif: # list topcs
-        #   pass
-        # invocar callback
-        # else:
+        elif msg.command == "listTopics":
+            # invocar callback
+            pass
 
     def list_topics(self, callback: Callable):
         """Lists all topics available in the broker."""
-        CDProto.send_msg(self.sock, CDProto.list_topics())  # ainda falta implementar
+        CDProto.send_msg(self.sock, Command.LIST_TOPICS, self.serializer, self.topic)
         callback()
 
     def cancel(self):
         """Cancel subscription."""
-        CDProto.send_msg(
-            self.sock, CDProto.cancel(self.topic)
-        )  # também falta implementar
+        CDProto.send_msg(self.sock, Command.UNSUBSCRIBE, self.serializer, self.topic)
 
 
 class JSONQueue(Queue):
     """Queue implementation with JSON based serialization."""
 
     def __init__(self, topic, _type=MiddlewareType.CONSUMER):
-        super().__init__(topic, _type)
-        self.serialization = Serializer.JSON
-
-    def push(self, value):
-        """Sends data to broker."""
-        CDProto.send_msg(
-            self.sock, Command.PUBLISH, self.serialization, self.topic, value
-        )
-
-    def pull(self) -> Tuple[str, Any]:
-        """Receives (topic, data) from broker. Should BLOCK the consumer!"""
-        msg = CDProto.recv_msg(self.sock)
-        if msg.command == "publish":
-            return msg
-        elif msg.command == "listTopics":
-            # invocar callback
-            pass
-        # else:
-
-    def list_topics(self, callback: Callable):
-        """Lists all topics available in the broker."""
-        CDProto.send_msg(self.sock, Command.LIST_TOPICS, self.serialization, self.topic)
-        callback()
-
-    def cancel(self):
-        """Cancel subscription."""
-        CDProto.send_msg(self.sock, Command.UNSUBSCRIBE, self.serialization, self.topic)
+        super().__init__(topic, Serializer.JSON, _type)
 
 
 class XMLQueue(Queue):
     """Queue implementation with XML based serialization."""
 
     def __init__(self, topic, _type=MiddlewareType.CONSUMER):
-        super().__init__(topic, _type)
-        self.serialization = Serializer.XML
-
-    def push(self, value):
-        """Sends data to broker."""
-        CDProto.send_msg(
-            self.sock, Command.PUBLISH, self.serialization, self.topic, value
-        )
-
-    def pull(self) -> Tuple[str, Any]:
-        """Receives (topic, data) from broker. Should BLOCK the consumer!"""
-        msg = CDProto.recv_msg(self.sock)
-        if msg.command == "publish":
-            return msg
-        elif msg.command == "listTopics":
-            # invocar callback
-            pass
-        # else:
-
-    def list_topics(self, callback: Callable):
-        """Lists all topics available in the broker."""
-        CDProto.send_msg(self.sock, Command.LIST_TOPICS, self.serialization, self.topic)
-        callback()
-
-    def cancel(self):
-        """Cancel subscription."""
-        CDProto.send_msg(self.sock, Command.UNSUBSCRIBE, self.serialization, self.topic)
+        super().__init__(topic, Serializer.XML, _type)
 
 
 class PickleQueue(Queue):
     """Queue implementation with Pickle based serialization."""
 
     def __init__(self, topic, _type=MiddlewareType.CONSUMER):
-        super().__init__(topic, _type)
-        self.serialization = Serializer.PICKLE
-
-    def push(self, value):
-        """Sends data to broker."""
-        CDProto.send_msg(
-            self.sock, Command.PUBLISH, self.serialization, self.topic, value
-        )
-
-    def pull(self) -> Tuple[str, Any]:
-        """Receives (topic, data) from broker. Should BLOCK the consumer!"""
-        msg = CDProto.recv_msg(self.sock)
-        if msg.command == "publish":
-            return msg
-        elif msg.command == "listTopics":
-            # invocar callback
-            pass
-        # else:
-
-    def list_topics(self, callback: Callable):
-        """Lists all topics available in the broker."""
-        CDProto.send_msg(self.sock, Command.LIST_TOPICS, self.serialization, self.topic)
-        callback()
-
-    def cancel(self):
-        """Cancel subscription."""
-        CDProto.send_msg(self.sock, Command.UNSUBSCRIBE, self.serialization, self.topic)
+        super().__init__(topic, Serializer.PICKLE, _type)
